3866c7f8b0c57e77ac8e3ac174acd5438dfbf9d0
Sergey Ryabin <ryabin.ss@gmail.com>
2024-08-30 08:56:34 +0000

    temp: disable pub/sub nats test and tests for mindwm xrd resource

--- tests/mindwm_tests/test_mindwm_user.py
+++ tests/mindwm_tests/test_mindwm_user.py
@@ -3,31 +3,6 @@ from mindwm import test_namespace
 
 username = "pion"
 
-class Test_MindwmUser(test_namespace):
-    namespace = f"user-{ username}"
+#class Test_MindwmUser(test_namespace):
+#    namespace = f"user-{ username}"
 
-#class Test_Nats(object):
-#    def test_ns(self, kube):
-#        namespaces = kube.get_namespaces()
-#        ns = namespaces.get(nats_namespace)
-#        assert ns is not None
-#
-#    @pytest.mark.depends(on=["test_ns"])
-#    def test_deployment(self, kube):
-#        deployments = kube.get_deployments(namespace=nats_namespace)
-#        nats_box = deployments.get(nats_deployment)
-#        assert nats_box is not None
-#        
-#    @pytest.mark.depends(on=["test_ns"])
-#    def test_statefulset(self, kube):
-#        statefulsets = kube.get_statefulsets(namespace=nats_namespace)
-#        nats = statefulsets.get(nats_statefulset)
-#        assert nats is not None
-#
-##
-##
-##def test_nats_namespace(kube):
-##    deployments = kube.get_deployments(namespace=nats_namespace)
-##    nats_box = deployments.get(nats_deployment)
-##    assert 0 and nats_box is not None
-##
