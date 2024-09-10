fa0a52a565e301c13bae8837666279709f038671
Ryabin Sergey <ryabin.ss@gmail.com>
2024-09-10 18:56:09 +0000

    feat: pytest dependencies and ordering

--- tests/mindwm_tests/test_crossplane.py
+++ tests/mindwm_tests/test_crossplane.py
@@ -1,6 +1,9 @@
 import pytest
 from mindwm import test_namespace
+import test_infra
 
+@pytest.mark.k8s_resources
+@pytest.mark.dependency(depends=['cluster'], scope = "package")
 class Test_Crossplane(test_namespace):
     namespace = "crossplane-system"
     deployment = [
--- tests/mindwm_tests/test_infra.py
+++ tests/mindwm_tests/test_infra.py
@@ -3,9 +3,9 @@ import subprocess
 import allure
 
 
-@pytest.mark.e2e
+@pytest.mark.gitops
 @allure.title("Test mindwm infra")
-class TestInfra:
+class TestInfra(object):
     def run_cmd(self, cmd):
         try:
             result = subprocess.run(["sh", "-c", cmd], check=True, text=True, capture_output=True, cwd="../../")
@@ -18,37 +18,39 @@ class TestInfra:
             pytest.fail(f"Make cluster command failed with return code {e.returncode}")
 
     @allure.story('Deploy k8s cluster')
+    @pytest.mark.dependency(name = "cluster")
     def test_cluster(self):
         self.run_cmd("make cluster")
        
 
     @allure.story('Create argocd mindwm application')
-    @pytest.mark.depends(on=['test_cluster'])
+    @pytest.mark.dependency(name = 'argocd_app', depends=['cluster'], scope = "session")
     def test_argocd_app(self):
         self.run_cmd("make argocd_app")
 
     @allure.story('Argocd mindwm application sync')
-    @pytest.mark.depends(on=['test_argocd_app'])
+    @pytest.mark.dependency( name = 'argocd_app_sync_async', depends=['argocd_app'], scope = "session")
     def test_argocd_app_sync_async(self):
         self.run_cmd("make argocd_app_sync_async")
 
 
     @allure.story('Waiting for argocd mindwm application is ready')
-    @pytest.mark.depends(on=['test_argocd_app_sync_async'])
+    @pytest.mark.dependency( name = 'argocd_app_async_wait', depends=['argocd_app_sync_async'], scope = "session")
     def test_argocd_app_async_wait(self):
         self.run_cmd("make argocd_app_async_wait")
 
     @allure.story('Crossplane rolebinding workaround')
-    @pytest.mark.depends(on=['test_argocd_app_async_wait'])
+    @pytest.mark.dependency( name = 'crossplane_rolebinding_workaround', depends=['argocd_app_async_wait'], scope = "session")
     def test_crossplane_rolebinding_workaround(self):
         self.run_cmd("make crossplane_rolebinding_workaround")
 
     @allure.story('Verify that everthing is green in argocd')
-    @pytest.mark.depends(on=['test_crossplane_rolebinding_workaround'])
+    @pytest.mark.dependency( name = 'argocd_apps_ensure', depends=['crossplane_rolebinding_workaround'], scope = "session")
     def test_argocd_apps_ensure(self):
         self.run_cmd("make argocd_apps_ensure")
 
     @allure.story('Create mindwm resources, context, user, host')
-    @pytest.mark.depends(on=['test_argocd_apps_ensure'])
+    @pytest.mark.dependency( name = 'mindwm_resoures', depends=['argocd_apps_ensure'], scope = "session")
     def test_mindwm_resoures(self):
         self.run_cmd("make mindwm_resources")
+
