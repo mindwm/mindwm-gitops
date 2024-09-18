7202574d7b0b9ceea8b31b560eaa49055b3a919a
Sergey Ryabin <ryabin.ss@gmail.com>
2024-09-18 14:58:20 +0200

    feat: exit instantly on first error or failed test

--- tests/mindwm_bdd/run_tests.sh
+++ tests/mindwm_bdd/run_tests.sh
@@ -3,4 +3,4 @@ ARTIFACT_DIR=artifact_dir
 source .venv/bin/activate
 . .venv/bin/activate
 #pytest -s --md-report --md-report-tee --md-report-verbose=7  --md-report-tee --md-report-output=${ARTIFACT_DIR}/report.md --kube-config=${HOME}/.kube/config --alluredir ${ARTIFACT_DIR}/allure-results . --order-dependencies
-pytest -s --disable-warnings --no-header -vv --gherkin-terminal-reporter --kube-config=${HOME}/.kube/config --alluredir=${ARTIFACT_DIR}/allure-results 
+pytest -s -x --disable-warnings --no-header -vv --gherkin-terminal-reporter --kube-config=${HOME}/.kube/config --alluredir=${ARTIFACT_DIR}/allure-results
