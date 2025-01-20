c2a035b06578bca8267997ab4c35981417734d18
Ryabin Sergey <ryabin.ss@gmail.com>
2025-01-20 16:41:46 +0000

    fix: Setting with .default prefix found; remove it.
    However, we can workaround this by placing all of (1) under a specific key (.Values.defaults).
    We can then merge the profile onto the defaults, then the user settings onto that.
    Finally, we can set all of that under .Values so the chart behaves without awareness.

--- tests/mindwm_bdd/run_tests.sh
+++ tests/mindwm_bdd/run_tests.sh
@@ -2,6 +2,7 @@
 ARTIFACT_DIR=artifact_dir
 source .venv/bin/activate
 . .venv/bin/activate
+pip3 install -r requirements.txt
 #pytest -s --md-report --md-report-tee --md-report-verbose=7  --md-report-tee --md-report-output=${ARTIFACT_DIR}/report.md --kube-config=${HOME}/.kube/config --alluredir ${ARTIFACT_DIR}/allure-results . --order-dependencies
 #pytest -s -x -m two_hosts_one_user --disable-warnings --no-header -vv --gherkin-terminal-reporter --kube-config=${HOME}/.kube/config --alluredir=${ARTIFACT_DIR}/allure-results
 pytest -s -m "$*" -x --disable-warnings --no-header -vv --gherkin-terminal-reporter --kube-config=${HOME}/.kube/config --alluredir=${ARTIFACT_DIR}/allure-results
