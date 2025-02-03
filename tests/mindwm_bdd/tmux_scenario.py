118c48805dfb457ad2f676563ab0be3b78e13a1d
Ryabin Sergey <ryabin.ss@gmail.com>
2025-02-03 17:43:31 +0000

    feat: capture pane content and save it to a file

--- tests/mindwm_bdd/tmux_scenario.py
+++ tests/mindwm_bdd/tmux_scenario.py
@@ -1,8 +1,9 @@
 from git_utils import git_clone
-from make import run_cmd
+from utils import run_cmd
 from tmux import create_tmux_session
 from tmux import send_command_to_pane
 from tmux import vertically_split_window
+from tmux import capture_pane
 import time
 
 work_dir="/tmp/mindwm-manager"
@@ -19,12 +20,13 @@ mindwm_manager_env = {
 } 
 hostname = "$(hostname -s)"
 
-git_clone(work_dir, "https://github.com/mindwm/mindwm-manager", "master", "HEAD")
+#git_clone(work_dir, "https://github.com/mindwm/mindwm-manager", "master", "HEAD")
 # #
 run_cmd("python3.11 -m venv .venv", work_dir)
-# run_cmd(f"{work_dir}/.venv/bin/pip install -r requirements.txt", work_dir)
+#run_cmd(f"{work_dir}/.venv/bin/pip install -r requirements.txt", work_dir)
 # exit(0)
 create_tmux_session(tmux_session_name, window_name, work_dir)
+capture_pane("/tmp/xxx", tmux_session_name, window_name, 0)
 send_command_to_pane(tmux_session_name, window_name, 0, ". .venv/bin/activate")
 for env_var in mindwm_manager_env.keys():
     send_command_to_pane(tmux_session_name, window_name, 0, f"export {env_var}={mindwm_manager_env[env_var]}")
