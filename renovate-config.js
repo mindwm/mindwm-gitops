82621862cc9354bd18d6141827fbb3146617bb05
Sergey Ryabin <ryabin.ss@gmail.com>
2025-02-11 19:09:54 +0100

    feat(renovate): configure helm manager

--- renovate-config.js
+++ renovate-config.js
@@ -1,5 +1,8 @@
 module.exports = {
   kubernetes: {
     fileMatch: ['main.yaml$', 'manifests/*.*', 'observability/.*'],
-  }
+  },
+  "helm-requirements": {
+    fileMatch: ['helm_requirements.yaml$'],
+  } 
 }
