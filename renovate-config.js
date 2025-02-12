1dc6d20e1101194ed1e891a29ea5e4f96890251e
Sergey Ryabin <ryabin.ss@gmail.com>
2025-02-12 01:12:14 +0100

    feat(renovate): add regex for crossplane dependencies

--- renovate-config.js
+++ renovate-config.js
@@ -1,11 +1,22 @@
 module.exports = {
   kubernetes: {
-    fileMatch: ['main.yaml$'],
-  }
+    fileMatch: ['./main.yaml$'],
+  },
   "argocd": {
-    "fileMatch": ["main.yaml$"]
-  }
+    "fileMatch": ["./main.yaml$"]
+  },
   "crossplane": {
-    "fileMatch": ["main.yaml$"]
-  }
+    "fileMatch": ["./main.yaml$"]
+  },
+  regexManagers: [
+    {
+      description: "patches crossplane provider package version  https://regex101.com/r/3ug5iv/3",
+      fileMatch: ["./main.yaml$"],
+      matchStrings: [
+        "apiVersion: pkg.crossplane.io\\/.*\\nkind: Provider\\n(.*\\n)+(.* package:[ ]*\"?)(?<depName>[a-z0-9\\.\\/\\-]*?):(?<currentValue>[a-z0-9\\.\\/\\-\\+]*?)\"?\\n"
+      ],
+      datasourceTemplate: "docker",
+      versioningTemplate: "docker"
+    },
+]
 }
