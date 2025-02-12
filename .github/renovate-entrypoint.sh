db40f2e5ee7401453afd9d1dd174990bfb20472d
Sergey Ryabin <ryabin.ss@gmail.com>
2025-02-12 02:56:07 +0100

    fix(renovate): call kcl run from the git-templates

--- .github/renovate-entrypoint.sh
+++ .github/renovate-entrypoint.sh
@@ -2,8 +2,11 @@
 
 apt update
 bash <(curl -sL https://kcl-lang.io/script/install-cli.sh) 0.9.7
-echo ---
-find / | grep -i main.k
-echo ---
+mkdir -p ~/.git-templates/hooks
+cat<<EOF | tee ~/.git-templates/hooks/post-checkout
+#!/bin/bash
 kcl run -o main.yaml
+EOF
+chmod +x ~/.git-templates/hooks/post-checkout
+git config --global init.templateDir ~/.git-templates
 runuser -u ubuntu renovate
