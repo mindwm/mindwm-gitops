bf94646328f85e2efe0346024a3b93b31d3cd658
Sergey Ryabin <ryabin.ss@gmail.com>
2025-05-12 11:42:53 +0200

    test: fake test

--- tests/mindwm_bdd/features/xxx_fake_test.feature
+++ tests/mindwm_bdd/features/xxx_fake_test.feature
@@ -2,7 +2,3 @@
 Feature: Fake test
     Scenario: Fake test
       Then the directory '/tmp' should exist
-      When God creates a tmux session named 'test' with a window named 'test'
-      Then the tmux session 'test' should exist
-      Then the directory '/tmp2' should exist
-      Then the tmux session 'test' should exist
--- tests/mindwm_bdd/run_tests.sh
+++ tests/mindwm_bdd/run_tests.sh
@@ -4,6 +4,10 @@ ARTIFACT_DIR=artifact_dir
 #pip3 install -r requirements.txt
 #pytest -s --md-report --md-report-tee --md-report-verbose=7  --md-report-tee --md-report-output=${ARTIFACT_DIR}/report.md --kube-config=${HOME}/.kube/config --alluredir ${ARTIFACT_DIR}/allure-results . --order-dependencies
 #pytest -s -x -m two_hosts_one_user --disable-warnings --no-header -vv --gherkin-terminal-reporter --kube-config=${HOME}/.kube/config --alluredir=${ARTIFACT_DIR}/allure-results
+
+
 pytest -s -x -m "$*" --junit-xml=${ARTIFACT_DIR}/report.xml --log-level info --disable-warnings --no-header -vv --gherkin-terminal-reporter --kube-config=${HOME}/.kube/config --alluredir=${ARTIFACT_DIR}/allure-results
+
+allure generate -c artifact_dir/allure-results && allure serve --host 0.0.0.0 --port 8080 allure-report
 
 # (time ./run_tests.sh eda) && (allure generate -c artifact_dir/allure-results && allure serve --host 0.0.0.0 --port 8080 allure-report)
